improve training and fix loss computation

pyrros/algorithms/grpo/grpo_sampler.py
@@ -1,29 +1,36 @@
-from typing import Sequence, Callable, List, Any, Union
+from typing import List
 
-from peft import PeftModel
 import torch
-import torch.nn.functional as F
-from composer.core import Algorithm, Event, State
+from composer.core import Algorithm, Event
 
-from transformers import GenerationConfig, PreTrainedModel, PreTrainedTokenizer
-
-from pyrros.models.grpo_model import GRPOModel
-
+from transformers import GenerationConfig, PreTrainedTokenizer
 
 class GRPOSampler(Algorithm):
-    def __init__(self, tokenizer: PreTrainedTokenizer, reward_fns, G):
+    def __init__(self, tokenizer: PreTrainedTokenizer, reward_fns, G, generation_kwargs: dict = None):
         self.tokenizer = tokenizer
         self.reward_fns = reward_fns
         self.G = G
+        self.generation_kwargs = generation_kwargs or {}
 
     def match(self, event, state):
         return event == Event.BEFORE_FORWARD
 
-    def _compute_rewards(self, completions: List[str]) -> torch.Tensor:
-        list_of_rewards = [reward_fn(completions) for reward_fn in self.reward_fns]
+    def _compute_rewards(self, completions: List[str], completions_ids, prompts, answers,) -> torch.Tensor:
+        list_of_rewards = [
+            reward_fn(
+                completions=completions,
+                completions_ids=completions_ids,
+                prompts=prompts,
+                answers=answers,
+            ) for reward_fn in self.reward_fns]
+        # Convert list of rewards to tensor
+        list_of_rewards = [
+            torch.tensor(rewards, dtype=torch.float32) for rewards in list_of_rewards
+        ]
+
         return torch.stack(list_of_rewards, dim=1).sum(dim=1)
 
-    
+
     def _group_advantages(self, rewards: torch.Tensor, eps: float = 1e-8) -> torch.Tensor:
         return (rewards - rewards.mean()) / (rewards.std() + eps)
 
@@ -33,25 +40,33 @@ class GRPOSampler(Algorithm):
         ref_model = state.ref_model
         input_ids: torch.Tensor = state.batch["input_ids"]
         attention_mask: torch.Tensor = state.batch["attention_mask"]
+        prompts = state.batch["prompts"]
+        answers = state.batch["answers"]
+
 
         # duplicate prompt num_rollouts times
         input_ids = input_ids.repeat(self.G, 1)
         attention_mask = attention_mask.repeat(self.G, 1)
+        prompts = prompts * self.G
+        answers = answers * self.G
 
-        # 1. generate completions
-        pad_token_id = self.tokenizer.pad_token_id or self.tokenizer.eos_token_id
         generation_config = GenerationConfig(
+            pad_token_id=state.model.config.pad_token_id,
+            bos_token_id=state.model.config.bos_token_id,
+            eos_token_id=state.model.config.eos_token_id,
             do_sample=True,
-            top_p=1.0,
-            temperature=0.6,
-            max_length=128,
-            pad_token_id=pad_token_id,
-            output_logits=True,
             return_dict_in_generate=True,
+            # output_logits=True,
+
+            **self.generation_kwargs,
         )
+
+
         # old_model_output = self.old_model.generate(input_ids, generation_config=generation_config)
+        state.model.eval()
         old_model_output = state.model.generate(input_ids, generation_config=generation_config)
-        
+        state.model.train()
+
         sequence_ids = old_model_output.sequences
 
         completions = self.tokenizer.batch_decode(
@@ -61,34 +76,47 @@ class GRPOSampler(Algorithm):
 
         completion_mask = torch.zeros_like(sequence_ids, dtype=torch.bool)
         completion_mask[:, input_ids.shape[1] :] = True
-        completion_mask[sequence_ids == pad_token_id] = False
+        completion_mask[sequence_ids == self.tokenizer.pad_token_id] = False
         completion_mask = completion_mask[:, 1:]
 
 
         # 2. compute rewards and advantages
-        rewards = self._compute_rewards(completions)
+        rewards = self._compute_rewards(
+            completions=completions,
+            completions_ids=sequence_ids[:, input_ids.shape[1]:],
+            prompts=prompts,
+            answers=answers,
+        )
         advantages = self._group_advantages(rewards)
         advantages = advantages.to(input_ids.device)
 
         # 3. compute log-probabilities
-        attention_mask = sequence_ids != pad_token_id
+        attention_mask = sequence_ids != self.tokenizer.pad_token_id
         with torch.no_grad():
-            logp_ref = ref_model.compute_log_probs(sequence_ids, attention_mask, gen_mask=completion_mask)
-        
-            logits = torch.stack(old_model_output.logits, dim=1)
-            # 2. log-probas π_old sur les tokens générés
-            logp_old = torch.log_softmax(logits, dim=-1)
-            gen_ids  = sequence_ids[:, -logits.size(1):]             # tokens générés
-            logp_old = logp_old.gather(-1, gen_ids.unsqueeze(-1)).squeeze(-1)  # (B, L_gen)
+            logp_ref = ref_model.compute_log_probs(sequence_ids, attention_mask)
 
-        
+            # logits = torch.stack(old_model_output.logits, dim=1)
+            # # 2. log-probas π_old sur les tokens générés
+            # logp_old = torch.log_softmax(logits, dim=-1)
+            # gen_ids  = sequence_ids[:, -logits.size(1):]             # tokens générés
+            # logp_old = logp_old.gather(-1, gen_ids.unsqueeze(-1)).squeeze(-1)  # (B, L_gen)
+
+            # B, total_len = sequence_ids.shape
+            # L_gen = old_model_output.logits[0].size(1)   # ou logits.size(1) après stack
+            # print("sequence_ids:", sequence_ids.shape)
+            # print("logits:", logits.shape)
+            # print("completion_mask:", completion_mask.shape)
+            # print("— prompt_len:", input_ids.shape[1], "gen_len:", L_gen)
+            # print("true par batch:", completion_mask.sum(dim=1))
+
+
         state.batch = {
             "input_ids": input_ids,
             "labels": state.batch["labels"],
             "attention_mask": attention_mask,
 
             "sequence_ids": sequence_ids,  # (B,G,L)
-            "logprobs_old": logp_old,  # (B,G,V)
+            # "logprobs_old": logp_old,  # (B,G,V)
             "logprobs_ref": logp_ref,  # (B,G,V)
             "advantages": advantages,  # (B,G)
             "completion_mask": completion_mask,  # (B,G,L)

pyrros/models/grpo_model.py
@@ -1,8 +1,6 @@
 from typing import Union
 from composer.models import HuggingFaceModel
-from peft import PeftModel
 import torch, torch.nn.functional as F
-import transformers
 
 class GRPOModel(HuggingFaceModel):
     def __init__(self, model, tokenizer, epsilon=0.2, beta=0.02):
@@ -12,20 +10,21 @@ class GRPOModel(HuggingFaceModel):
     def forward(self, batch):
         sequence_ids = batch["sequence_ids"]
         attention_mask = batch["attention_mask"]
-        completion_mask = batch["completion_mask"]
-        log_probs = self.compute_log_probs(sequence_ids, attention_mask, gen_mask=completion_mask)
+        log_probs = self.compute_log_probs(sequence_ids, attention_mask)
         return log_probs
 
     def loss(self, outputs, batch):
         log_probs = outputs
-        logprobs_old = batch["logprobs_old"]
+        # logprobs_old = batch["logprobs_old"]
         logprobs_ref = batch["logprobs_ref"]
         advantages = batch["advantages"]
         completion_mask = batch["completion_mask"]
-        completion_mask = completion_mask[:, -logprobs_old.size(1):]
+        # completion_mask = completion_mask[:, -logprobs_old.size(1):]
 
         kl = self._approximate_kl_divergence(log_probs, logprobs_ref, completion_mask)
-        
+
+        logprobs_old = log_probs.detach()
+
         # Compute the loss
         ratio = (log_probs - logprobs_old).exp()
         surrogate_loss = ratio * advantages.unsqueeze(-1)
@@ -50,8 +49,7 @@ class GRPOModel(HuggingFaceModel):
     def compute_log_probs(
         self,
         sequence_ids: torch.Tensor,
-        attention_mask: torch.Tensor,
-        gen_mask: torch.Tensor | None = None,
+        attention_mask: torch.Tensor
     ) -> torch.Tensor:
         """
         Calcule les log-probabilités des tokens générés par le modèle.
@@ -71,15 +69,11 @@ class GRPOModel(HuggingFaceModel):
         )
         logits = output["logits"]
 
+
         logits = logits[:, :-1].to(torch.float32)
         output_ids = sequence_ids[:, 1:]
 
         log_probs = F.log_softmax(logits, dim=-1)
         log_probs = log_probs.gather(dim=-1, index=output_ids.unsqueeze(-1)).squeeze(-1)
-
-        if gen_mask is not None:
-            # shaped (B, L_total-1) → on compresse en (B, L_gen)
-            log_probs = log_probs.masked_select(gen_mask).view(log_probs.size(0), -1)
-
 
         return log_probs